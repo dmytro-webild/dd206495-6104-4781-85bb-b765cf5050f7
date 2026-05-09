ff2583c9b86d3b92660988a98c31503620e63bb1
dk <kudin.dima@gmail.com>
2026-05-09 22:21:34 +0000

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,49 @@
+import type { Metadata } from "next";
+import { Halant } from "next/font/google";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import "@/lib/gsap-setup";
+import { ServiceWrapper } from "@/components/ServiceWrapper";
+import Tag from "@/tag/Tag";
+import { getVisualEditScript } from "@/utils/visual-edit-script";
+import { DM_Sans } from "next/font/google";
+
+
+
+export const metadata: Metadata = {
+  title: 'XQP Esports | Growing Community',
+  description: 'XQP is a growing esports and gaming community focused on content creation, tournaments, and building one of the strongest gaming communities in South Africa.',
+  openGraph: {
+    "title": "XQP Esports",
+    "description": "The fastest growing gaming community.",
+    "siteName": "XQP Esports",
+    "type": "website"
+  },
+};
+
+const dmSans = DM_Sans({
+  variable: "--font-dm-sans",
+  subsets: ["latin"],
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <ServiceWrapper>
+        <body className={`${dmSans.variable} antialiased`}>
+          <Tag />
+          {children}
+          <script
+              dangerouslySetInnerHTML={{
+                  __html: `${getVisualEditScript()}`
+              }}
+          />
+        </body>
+      </ServiceWrapper>
+    </html>
+  );
+}
